30e19a52163a840d7d72e17ac4798a65b3c67d08
JoseLozadaAD <jose.lozada@training.assuresoft.com>
2023-11-16 17:37:05 -0400

    test-dev

--- src/main/java/com/assuresoft/rickandmorty/exceptions/EntityNotFoundException.java
+++ src/main/java/com/assuresoft/rickandmorty/exceptions/EntityNotFoundException.java
@@ -2,6 +2,7 @@ package com.assuresoft.rickandmorty.exceptions;
 
 public class EntityNotFoundException extends RuntimeException {
   public EntityNotFoundException(String message) {
+
     super(message);
   }
 }
