bfdbcc91f6b1e6f4de779c6259b637bde483409b
JoseLozadaAD <jose.lozada@training.assuresoft.com>
2023-11-20 16:24:02 -0400

    Change swagger 2 a swagger 3 dependency

--- src/main/java/com/assuresoft/rickandmorty/exceptions/EntityNotFoundException.java
+++ src/main/java/com/assuresoft/rickandmorty/exceptions/EntityNotFoundException.java
@@ -2,7 +2,6 @@ package com.assuresoft.rickandmorty.exceptions;
 
 public class EntityNotFoundException extends RuntimeException {
   public EntityNotFoundException(String message) {
-
     super(message);
   }
 }
